e1e815d9883a7461fb8e5c8aad2eeadcad149dd7
user <user@userde-macbook-8.local>
2017-06-13 10:04:07 +0800

    v4

--- Lee07/main.swift
+++ Lee07/main.swift
@@ -20,12 +20,37 @@ print("Hello, World!")
 //    print("XX")
 //}
 
+//閉包用法
+//0
 func mysort(a:Int,b:Int) -> Bool{
-    return true
+    print("\(a):\(b)")
+    return a<b
 }
 
 print(type(of:mysort))
 
-let a1 = [3,2,1,5,63,3,52]
+let a1 = [3,2,1,5,]
 let a2 = a1.sorted(by: mysort)
 print(a2.description)
+
+//1
+var a3 = a1.sorted(by:
+    {(a,b) -> Bool in
+        print("\(a):\(b)")
+        return a < b
+})
+print(a3.description)
+
+
+//2
+var a4 = a1.sorted(by:
+{(a,b) -> Bool in a < b})
+print(a4.description)
+
+//3
+var a5 = a1.sorted(by:{$0 < $1}) //用位置
+print(a5.description)
+
+//3
+var a6 = a1.sorted(by:< )
+print(a6.description)
